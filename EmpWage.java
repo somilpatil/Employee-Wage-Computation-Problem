e5f650c4103814cbe56a3eea36d85cc7b4e17cec
[somilpatil] <[patilsomil118@gmail.com]>
2021-10-16 22:35:04 +0530

    Added: CalculateDailyWage

--- EmpWage.java
+++ EmpWage.java
@@ -2,12 +2,14 @@ package com.bl;
 
 import java.util.Random;
 public class EmpWage  {
-
 	public static void main(String[] args) {
 		Random rd = new Random();
 		int empcheck = rd.nextInt(2);
+		int wagePerHour = 20;
+		int workHrs = 8;
 		if(empcheck==1) {
 			System.out.println("Employee is Present");
+			System.out.println(wagePerHour * workHrs);
 		}
 		else {
 			System.out.println("Employee is Absent");
