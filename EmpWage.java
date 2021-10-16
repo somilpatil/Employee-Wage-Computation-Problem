1fe740a4e332967e64639aa0a60b1da45263bcc5
[somilpatil] <[patilsomil118@gmail.com]>
2021-10-16 22:21:30 +0530

    Added: CheckAttendace

--- EmpWage.java
+++ EmpWage.java
@@ -4,7 +4,6 @@ import java.util.Random;
 public class EmpWage  {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to Employee Wage Computation Program");
 		Random rd = new Random();
 		int empcheck = rd.nextInt(2);
 		if(empcheck==1) {
