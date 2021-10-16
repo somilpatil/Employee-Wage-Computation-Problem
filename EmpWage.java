2c2d70de8487554ca654224ac33af89f4623d514
[somilpatil] <[patilsomil118@gmail.com]>
2021-10-17 00:12:30 +0530

    Added: SwitchCaseStatement

--- EmpWage.java
+++ EmpWage.java
@@ -5,18 +5,19 @@ public class EmpWage  {
 	public static void main(String[] args) {
 		Random rd = new Random();
 		int empcheck = rd.nextInt(3);
-		int wagePerHr= 20;
-		int fullTimeWorkHrs= 8;
-		int partTimeWorkHrs= 4;
-		if(empcheck==1) {
-			System.out.println("FullTime Wage");
-			System.out.println( wagePerHr * fullTimeWorkHrs );
-		}
-		else if(empcheck==2) {
-			System.out.println("PartTime Wage");
-			System.out.println( wagePerHr * partTimeWorkHrs);
-		}
-		else {
+		int wagePerHr = 20;
+		int fullTimeWorkHrs = 8;
+		int partTimeWorkHrs = 4;
+		switch(empcheck) {
+		case 1:
+			System.out.println("Salary of Fulltime Employee");
+			System.out.println( wagePerHr * fullTimeWorkHrs);
+		break;
+		case 2:
+			System.out.println("Salary of Parttime Employee");
+			System.out.println( wagePerHr * partTimeWorkHrs );
+		break;
+		default:
 			System.out.println("Salary = 0");
 		}
 	}
